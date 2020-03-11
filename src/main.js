3568d21a891872188216bb7b08a0e4a539d52e31
vantutrieu97 <vantutrieu97@gmail.com>
2020-03-11 10:15:53 +0700

    Install VueX

--- src/main.js
+++ src/main.js
@@ -1,15 +1,32 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import 'es6-promise/auto'
 
-Vue.config.productionTip = false
+
+Vue.use(Vuex);
+
+Vue.config.productionTip = false;
+
+const store = new Vuex.Store({
+  state: {
+    count: 10
+  }
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  store,
+  components: {App},
+  template: '<App/>',
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  }
+});
